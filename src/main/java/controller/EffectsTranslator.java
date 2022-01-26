f9347ab5ccc1c33982c97c0446c2e972447be25a
Davida Garner <davidac.garner@gmail.com>
2022-01-26 17:11:46 -0500

    Created EffectTranslator.java , AB#279
    Created EffectTranslator.java

---
+++ src/main/java/controller/EffectsTranslator.java
@@ -0,0 +1,36 @@
+package controller;
+
+import models.Person;
+
+public class EffectTranslator {
+
+    public void doEffect(Person user, String effect){
+
+        effect = "partner:30000";
+
+        String[] parsed = effect.split(":");
+
+        String action = parsed[0];
+        int value = Integer.parseInt(parsed[1]);
+
+
+        switch(action){
+            case "partner":
+                user.changePartner(value);
+                break;
+            case "money":
+                user.addMoney(effect);
+                break;
+            case "health":
+                user.addHealth(effect);
+                break;
+
+        }
+    }
+
+    void modifyPartner(Person user, String effect){
+
+    }
+}
+
+
