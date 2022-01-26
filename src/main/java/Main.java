b3e3573ad8f7827a666c74d3f7fdfcb17b0162b0
Dominic <dominicdeantonio@icloud.com>
2022-01-26 13:51:39 -0500

    Set up basic structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import org.json.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        JSONObject json = new JSONObject("{\"hello\":\"this is some text\"}");
+        System.out.println(json.get("hello"));
     }
-
 }
---
+++ src/main/java/view/Renderer.java
@@ -0,0 +1,11 @@
+package view;
+
+import controller.Game;
+
+public class Renderer {
+
+    static void render(Game game) {
+
+    }
+
+}
