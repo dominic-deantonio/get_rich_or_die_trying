527c4198c75753f1b1a9ed14f65ffa2c703910f9
Dominic <dominicdeantonio@icloud.com>
2022-01-27 16:29:51 -0500

    Continued work on effectstranslator and started person class, AB#253, AB#247

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,20 +1,34 @@
 package models;
 
-import java.lang.reflect.Field;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Locale;
 
 public class Person {
 
+    private int netWorth = 0, health = 100;
+    private final NumberFormat money = NumberFormat.getCurrencyInstance();
+
     Boolean education = false;
-    Career career = Career.DANGER;
+    Career career = Career.PASSION;
+    Person partner = null;
+    String name;
+
+    public Person() {
+        money.setMaximumFractionDigits(0);
+    }
+
+    public Person(String name, int initialWorth) {
+        this.name = name;
+        this.netWorth = initialWorth;
+    }
 
     public int getHealthPoints() {
         return 0;
     }
 
     public int getNetWorth() {
-        return 0;
+        return netWorth;
     }
 
     public String getPrettyNetWorth() {
@@ -25,8 +39,54 @@ public class Person {
         return false;
     }
 
-    public String getCategoryValue(int index){
+    public String getCategoryValue(int index) {
         List<String> fieldValues = List.of(career.toString().toLowerCase(), education.toString());
         return fieldValues.get(index);
+    }
+
+    public void addMoney(int value) {
+        netWorth += value;
+        final String msg = String.format("You have %s %s", (value < 0 ? "lost" : "gained"), money.format(value));
+        System.out.println(msg);
+    }
+
+    public void addHealth(int value) {
+        health += value;
+        if (health > 100)
+            health = 100;
+        String msg = String.format("You have gained %d health", value);
+        System.out.println(msg);
+
+    }
+
+    public void addPartner(int value) {
+        if (partner == null)
+            partner = new Person("Janine", value);
+
+        final String msg = String.format("You have a new partner named %s", partner.name);
+        System.out.println(msg);
+
+    }
+
+    public void breakUp(int value) {
+        System.out.println("Should break up with partner");
+
+    }
+
+    public void marryPartner(int value) {
+        System.out.println("Should marry partner and get dowry of " + value);
+
+    }
+
+    public void addChild(int value) {
+        System.out.println("Should add children: " + value);
+
+    }
+
+    public void changeCareer(int value) {
+        String oldCareer = career.name();
+        career = Career.values()[value];
+        final String msg = String.format("Your career has changed from %s to %s", oldCareer, career.toString());
+        System.out.println(msg);
     }
 }
